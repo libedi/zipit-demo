001d6dac9e03e42d38b125fed239e9a9c7245efe
libedi@gmail.com <libedi@gmail.com@dfce5bb4-af9e-2e16-6b87-cd6e38002a16>
2014-07-16 05:59:58 +0000

    불필요한 코드 삭제

--- trunk/zipit-demo/src/com/sujiewon/demo/dao/DemoDaoImpl.java
+++ trunk/zipit-demo/src/com/sujiewon/demo/dao/DemoDaoImpl.java
@@ -3,7 +3,6 @@ package com.sujiewon.demo.dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.HashMap;
 
 import org.apache.commons.logging.Log;
